73f194c73f879706e5aeabdf3720255d1509f9fc
Bobby <qq1499412503@gmail.com>
2021-06-07 21:50:38 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 uwsgi --ini /SOM_PROJECT/SOM_PROJECT/uwsgi.ini
-chmod 777 /SOM_PROJECT/SOM_PROJECT.sock
 cp /SOM_PROJECT/nginx.conf /etc/nginx/sites-enabled/
 service mongodb start
 service nginx restart
